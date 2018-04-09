58e1ed196a6bd1510b97f34202f31477142ef073
Pham Quang Long <longpq18@gmail.com>
2018-04-09 16:16:42 +0700

    add Access-Control-Allow-Headers

--- config/app.js
+++ config/app.js
@@ -2,5 +2,12 @@ var express = require('express')
 var app = express()
 var db = require('./db.config')
 var UserController = require('./../api/controllers/user.controller');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/users', UserController);
 module.exports = app
--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var app = require('./config/app');
+var http = require('http');
+
+// http.createServer(function (request, response) {
+// response.writeHead(200, {
+//     'Content-Type': 'text/plain',
+//     'Access-Control-Allow-Origin' : '*',
+//     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+// });
+
 var port = process.env.PORT || 4000;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
