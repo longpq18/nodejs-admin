3ab9b96c158908af1e05bc29f92571abc261dd37
Pham Quang Long <longpq18@gmail.com>
2018-04-11 15:37:45 +0700

    add Asset-Control-Allow-Methods

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -38,7 +38,7 @@ router.get('/', function (req, res) {
 router.delete('/delete/:id', function (req, res) {
     User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User "+ user.email +" was deleted.");
+        res.status(200).send("User was deleted.");
     });
 });
 
--- config/app.js
+++ config/app.js
@@ -6,6 +6,7 @@ var UserController = require('./../api/controllers/user.controller');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
